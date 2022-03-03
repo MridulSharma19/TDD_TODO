97d111c1683d19337f2504108093bb137d21c7cf
Mridulsharma19 <mridul906@gmail.com>
2022-03-03 19:44:57 +0530

    extracted functions to higher level

--- components/homepage/homepage.tsx
+++ components/homepage/homepage.tsx
@@ -5,37 +5,15 @@ import NewTaskInput from "./newTaskInput";
 import SubHeading from "./subheading";
 import TodoList from "./todoList";
 
-const Homepage = () => {
-    const [showAdd, setShowAdd] = useState(false);
-    const [tasks, setTasks] = useState<string[]>([]);
+interface Props {
+    tasks: string[];
+    handleRemove: Function;
+    handleAdd: Function;
+    showAdd: boolean;
+    handleClick: Function;
+}
 
-    const handleClick = () => {
-        setShowAdd(!showAdd);
-    };
-
-    const handleAdd = (newTask: string) => {
-        const taskList = tasks;
-        const newList = [...taskList, newTask];
-        setTasks(newList);
-        localStorage.setItem("taskList", JSON.stringify(newList));
-        setShowAdd(false);
-    };
-
-    const handleRemove = (task: string) => {
-        const taskList = tasks;
-        const index = taskList.indexOf(task);
-        taskList.splice(index, 1);
-        setTasks([...taskList]);
-        localStorage.setItem("taskList", JSON.stringify(taskList));
-    };
-
-    useEffect(() => {
-        const taskList = localStorage?.getItem("taskList");
-        if (taskList) {
-            setTasks(JSON.parse(taskList));
-        }
-    }, []);
-
+const Homepage = ({ tasks, handleRemove, handleAdd, showAdd, handleClick }: Props) => {
     return (
         <div className="home-container">
             <Heading />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,51 @@
 import Head from "next/head";
 
 import Homepage from "@/components/homepage/homepage";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [showAdd, setShowAdd] = useState(false);
+    const [tasks, setTasks] = useState<string[]>([]);
+
+    const handleClick = () => {
+        setShowAdd(!showAdd);
+    };
+
+    const handleAdd = (newTask: string) => {
+        const taskList = tasks;
+        const newList = [...taskList, newTask];
+        setTasks(newList);
+        localStorage.setItem("taskList", JSON.stringify(newList));
+        setShowAdd(false);
+    };
+
+    const handleRemove = (task: string) => {
+        const taskList = tasks;
+        const index = taskList.indexOf(task);
+        taskList.splice(index, 1);
+        setTasks([...taskList]);
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+    };
+
+    useEffect(() => {
+        const taskList = localStorage?.getItem("taskList");
+        if (taskList) {
+            setTasks(JSON.parse(taskList));
+        }
+    }, []);
     return (
         <div className="container">
             <Head>
                 <title>Todo App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Homepage />
+            <Homepage
+                tasks={tasks}
+                handleAdd={handleAdd}
+                handleClick={handleClick}
+                handleRemove={handleRemove}
+                showAdd={showAdd}
+            />
         </div>
     );
 }
